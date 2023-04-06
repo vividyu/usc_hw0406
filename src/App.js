3f2da39623bc0fee7d7197c3dff63a2393a2838b
Samuel <sixgod666@gmail.com>
2023-04-06 11:59:47 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <MyTabs>
       <div title="Section title 1">Content of section 1</div>
       <div title="Section title 2">Content of section 2</div>
-      <div title="Section title 3">Content of section 2</div>
+      <div title="Section title 3">Content of section 3</div>
     </MyTabs>
   );
 }
